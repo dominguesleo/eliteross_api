3fc9f7ac4e13f93afc3d94c36d3e3157e4562d03
Leonardo D <8dominguezleo@gmail.com>
2024-08-26 19:40:20 +0200

    WIP: User Models and Admin page

--- models/admin.py
+++ models/admin.py
@@ -3,22 +3,22 @@ from models.models.user_models import Client
 
 @admin.register(Client)
 class ClientAdmin(admin.ModelAdmin):
-    list_display = ('name', 'last_name', 'date_of_birth', 'phone', 'email', 'profession', 'fitness_goal')
-    list_filter = ('is_smoker', 'cigarette_per_day', 'is_diabetic', 'diabetic_type', 'physical_activity_at_work', 'is_physically_active', 'level_of_physical_activity', 'hours_of_sleep', 'is_active')
+    list_display = ('name', 'last_name', 'date_of_birth', 'phone', 'email', 'gender', 'profession', 'fitness_goal')
+    list_filter = ('gender', 'is_smoker', 'cigarette_per_day', 'is_diabetic', 'diabetic_type', 'physical_activity_at_work', 'is_physically_active', 'level_of_physical_activity', 'hours_of_sleep', 'is_active')
     search_fields = ('name', 'last_name', 'date_of_birth', 'phone', 'email', 'address', 'profession', 'fitness_goal')
-    ordering = ('name', 'last_name', 'date_of_birth', 'phone', 'email', 'profession', 'fitness_goal')
+    ordering = ('name', 'last_name', 'date_of_birth', 'phone', 'email', 'gender', 'profession', 'fitness_goal')
     readonly_fields = ('created_at', 'updated_at')
     fieldsets = (
-        ('Personal Information', {'fields': ('name', 'last_name', 'date_of_birth', 'phone', 'email', 'address', 'profession', 'physical_activity_at_work', 'fitness_goal')}),
-        ('Health Information', {'fields': ('is_smoker', 'cigarette_per_day', 'is_diabetic', 'diabetic_type', 'cholesterol', 'blood_pressure', 'heart_rate', 'altered_analytical_data', 'diseases', 'surgeries', 'injuries', 'contraindicated_physical_activity')}),
-        ('Sports, rest, and nutrition', {'fields': ('is_physically_active', 'physical_inactivity_per_month', 'physical_activity', 'physical_activity_per_week', 'level_of_physical_activity', 'days_available_for_training', 'hours_of_sleep', 'daily_meals', 'allergies_intolerances')}),
+        ('Personal Information', {'fields': ('name', 'last_name', 'date_of_birth', 'phone', 'email', 'gender', 'address', 'profession', 'physical_activity_at_work', 'fitness_goal')}),
+        ('Health Information', {'fields': ('is_smoker', 'cigarette_per_day', 'is_diabetic', 'diabetic_type', 'cholesterol', 'blood_pressure', 'heart_rate', 'altered_analytical_data', 'disease', 'surgery', 'injury', 'contraindicated_physical_activity')}),
+        ('Sports, rest, and nutrition', {'fields': ('is_physically_active', 'physical_inactivity_per_month', 'physical_activity', 'level_of_physical_activity', 'days_available_for_training', 'hours_of_sleep', 'daily_meals', 'allergies_intolerances')}),
         ('Tests and measurements', {'fields': ('biotype', 'shoulder_flexibility', 'hip_flexibility', 'knee_flexibility', 'ankle_flexibility')}),
         ('Admin Information', {'fields': ('created_at', 'updated_at', 'is_active')}),
     )
     add_fieldsets = (
-        ('Personal Information', {'fields': ('name', 'last_name', 'date_of_birth', 'phone', 'email', 'address', 'profession', 'physical_activity_at_work', 'fitness_goal')}),
-        ('Health Information', {'fields': ('is_smoker', 'cigarette_per_day', 'is_diabetic', 'diabetic_type', 'cholesterol', 'blood_pressure', 'heart_rate', 'altered_analytical_data', 'diseases', 'surgeries', 'injuries', 'contraindicated_physical_activity')}),
-        ('Sports, rest, and nutrition', {'fields': ('is_physically_active', 'physical_inactivity_per_month', 'physical_activity', 'physical_activity_per_week', 'level_of_physical_activity', 'days_available_for_training', 'hours_of_sleep', 'daily_meals', 'allergies_intolerances')}),
+        ('Personal Information', {'fields': ('name', 'last_name', 'date_of_birth', 'phone', 'email', 'gender', 'address', 'profession', 'physical_activity_at_work', 'fitness_goal')}),
+        ('Health Information', {'fields': ('is_smoker', 'cigarette_per_day', 'is_diabetic', 'diabetic_type', 'cholesterol', 'blood_pressure', 'heart_rate', 'altered_analytical_data', 'disease', 'surgery', 'injury', 'contraindicated_physical_activity')}),
+        ('Sports, rest, and nutrition', {'fields': ('is_physically_active', 'physical_inactivity_per_month', 'physical_activity', 'level_of_physical_activity', 'days_available_for_training', 'hours_of_sleep', 'daily_meals', 'allergies_intolerances')}),
         ('Tests and measurements', {'fields': ('biotype', 'shoulder_flexibility', 'hip_flexibility', 'knee_flexibility', 'ankle_flexibility')}),
     )
 
--- models/models/user_models.py
+++ models/models/user_models.py
@@ -3,43 +3,54 @@ from django.contrib.postgres.fields import ArrayField
 from django.core.validators import MinValueValidator, MaxValueValidator
 from datetime import date
 
+GENDER_CHOICES = [
+    ('Female', 'Female'),
+    ('Male', 'Male'),
+]
+
+PHYSICAL_ACTIVITY_CHOICES = [
+    ('Sedentary', 'Sedentary'),
+    ('Lightly Sedentary', 'Lightly Sedentary'),
+    ('Light', 'Light'),
+    ('Moderate', 'Moderate'),
+    ('Intense', 'Intense'),
+]
+
+CIGARETTE_CHOICES = [
+    ('1-5', '1-5'),
+    ('6-20', '6-20'),
+    ('+20', '+20'),
+]
+
+DIABETIC_CHOICES = [
+    ('Type 1', 'Type 1'),
+    ('Type 2', 'Type 2'),
+]
+
+SLEEP_CHOICES = [
+    ('-6 hours', '-6 hours'),
+    ('6-7 hours', '6-7 hours'),
+    ('8 hours', '8 hours'),
+    ('+9 hours', '+9 hours'),
+]
+
+MEALS_CHOICES = [
+    ('Breakfast', 'Breakfast'),
+    ('Mid-morning snack', 'Mid-morning snack'),
+    ('Lunch', 'Lunch'),
+    ('Mid-Afternoon snack', 'Mid-Afternoon snack'),
+    ('Dinner', 'Dinner'),
+    ('Evening snack', 'Evening snack'),
+    ('Others', 'Others'),
+]
+
+FLEXIBILITY_CHOICES = [
+    ('Good', 'Good'),
+    ('Regular', 'Regular'),
+    ('Bad', 'Bad'),
+]
+
 class Client(models.Model):
-
-    PHYSICAL_ACTIVITY_CHOICES = [
-        ('Sedentary', 'Sedentary'),
-        ('Lightly Sedentary', 'Lightly Sedentary'),
-        ('Light', 'Light'),
-        ('Moderate', 'Moderate'),
-        ('Intense', 'Intense'),
-    ]
-
-    CIGARETTE_CHOICES = [
-        ('1-5', '1-5'),
-        ('6-20', '6-20'),
-        ('+20', '+20'),
-    ]
-
-    DIABETIC_CHOICES = [
-        ('Type 1', 'Type 1'),
-        ('Type 2', 'Type 2'),
-    ]
-
-    SLEEP_CHOICES = [
-        ('-6 hours', '-6 hours'),
-        ('6-7 hours', '6-7 hours'),
-        ('8 hours', '8 hours'),
-        ('+9 hours', '+9 hours'),
-    ]
-
-    MEALS_CHOICES = [
-        ('Breakfast', 'Breakfast'),
-        ('Mid-morning snack', 'Mid-morning snack'),
-        ('Lunch', 'Lunch'),
-        ('Mid-Afternoon snack', 'Mid-Afternoon snack'),
-        ('Dinner', 'Dinner'),
-        ('Evening snack', 'Evening snack'),
-        ('Others', 'Others'),
-    ]
 
     # Personal Information
     name = models.CharField(max_length=100)
@@ -47,6 +58,7 @@ class Client(models.Model):
     date_of_birth = models.DateField()
     phone = models.CharField(max_length=20, null=True, blank=True)
     email = models.EmailField(null=True, blank=True)
+    gender = models.CharField(max_length=20, choices=GENDER_CHOICES)
     address = models.CharField(max_length=200, null=True, blank=True)
     profession = models.CharField(max_length=100, null=True, blank=True)
     physical_activity_at_work = models.CharField(max_length=100, null=True, blank=True, choices=PHYSICAL_ACTIVITY_CHOICES, default=None)
@@ -60,29 +72,19 @@ class Client(models.Model):
     cholesterol = models.FloatField(null=True, blank=True)
     blood_pressure = models.FloatField(null=True, blank=True)
     heart_rate = models.FloatField(null=True, blank=True)
-    altered_analytical_data = ArrayField(models.CharField(max_length=200), blank=True, default=list)
-    diseases = ArrayField(models.CharField(max_length=200), blank=True, default=list)
-    surgeries = ArrayField(models.CharField(max_length=200), blank=True, default=list)
-    injuries = ArrayField(models.CharField(max_length=200), blank=True, default=list)
-    contraindicated_physical_activity = ArrayField(models.CharField(max_length=200), blank=True, default=list)
 
     # Sports, rest, and nutrition
     is_physically_active = models.BooleanField(null=True)
     physical_inactivity_per_month = models.IntegerField(validators=[MinValueValidator(1)], null=True, blank=True)
-    physical_activity =ArrayField(models.CharField(max_length=200), blank=True, default=list)
-    physical_activity_per_week = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(7)], null=True, blank=True)
-    level_of_physical_activity = models.CharField(max_length=100, null=True, blank=True, choices=PHYSICAL_ACTIVITY_CHOICES, default=None)
     days_available_for_training = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(7)], null=True, blank=True)
     hours_of_sleep = models.CharField(max_length=100, null=True, blank=True, choices=SLEEP_CHOICES, default=None)
-    daily_meals = ArrayField(models.CharField(max_length=200, choices=MEALS_CHOICES), blank=True, default=list)
-    allergies_intolerances = ArrayField(models.CharField(max_length=200), blank=True, default=list)
 
     # Tests and measurements
     biotype = models.CharField(max_length=100, null=True, blank=True)
     shoulder_flexibility = models.FloatField(null=True, blank=True)
-    hip_flexibility = models.FloatField(null=True, blank=True)
-    knee_flexibility = models.FloatField(null=True, blank=True)
-    ankle_flexibility = models.FloatField(null=True, blank=True)
+    hip_flexibility = models.FloatField(null=True, blank=True, choices=FLEXIBILITY_CHOICES, default=None)
+    knee_flexibility = models.FloatField(null=True, blank=True, choices=FLEXIBILITY_CHOICES, default=None)
+    ankle_flexibility = models.FloatField(null=True, blank=True, choices=FLEXIBILITY_CHOICES, default=None)
 
     # Admin Information
     created_at = models.DateTimeField(auto_now_add=True)
@@ -95,15 +97,80 @@ class Client(models.Model):
         return today.year - self.date_of_birth.year - (
             (today.month, today.day) < (self.date_of_birth.month, self.date_of_birth.day)
         )
-
     def __str__(self):
         return f'{self.name} {self.last_name}'
+
+
+#* Health Information relationships
+
+class AlteredAnalyticalData(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='altered_analytical_data')
+    name = models.CharField(max_length=100)
+    reason = models.TextField(null=True, blank=True)
+    def __str__(self):
+        return f"{self.client.name} {self.client.last_name} - {self.name}"
+
+class Disease(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='disease')
+    name = models.CharField(max_length=100)
+    details = models.TextField(null=True, blank=True)
+    def __str__(self):
+        return f"{self.client.name} {self.client.last_name} - {self.name}"
+
+class Surgery(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='surgery')
+    name = models.CharField(max_length=100)
+    details = models.TextField(null=True, blank=True)
+    def __str__(self):
+        return f"{self.client.name} {self.client.last_name} - {self.name}"
+
+class Injury(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='injury')
+    name = models.CharField(max_length=100)
+    details = models.TextField(null=True, blank=True)
+    def __str__(self):
+        return f"{self.client.name} {self.client.last_name} - {self.name}"
+
+class ContraindicatedPhysicalActivity(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='contraindicated_physical_activity')
+    name = models.CharField(max_length=100)
+    details = models.TextField(null=True, blank=True)
+    def __str__(self):
+        return f"{self.client.name} {self.client.last_name} - {self.name}"
+
+
+#* Sports, rest, and nutrition
+
+class PhysicalActivity(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='physical_activity')
+    name = models.CharField(max_length=100)
+    per_week = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(7)], null=True, blank=True)
+    level = models.CharField(max_length=100, null=True, blank=True, choices=PHYSICAL_ACTIVITY_CHOICES, default=None)
+    details = models.TextField(null=True, blank=True)
+    def __str__(self):
+        return f"{self.client.name} {self.client.last_name} - {self.name}"
+
+class DailyMeals(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='daily_meals')
+    name = models.CharField(max_length=100)
+    details = models.TextField(null=True, blank=True)
+    def __str__(self):
+        return f"{self.client.name} {self.client.last_name} - {self.name}"
+
+class AllergiesIntolerances(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='allergies_intolerances')
+    name = models.CharField(max_length=100)
+    details = models.TextField(null=True, blank=True)
+    def __str__(self):
+        return f"{self.client.name} {self.client.last_name} - {self.name}"
+
+
+#* Tests and measurements
 
 class WeightRecord(models.Model):
     client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='weight_records')
     weight = models.FloatField()
     recorded_at = models.DateTimeField(auto_now_add=True)
-
     def __str__(self):
         return f"{self.client.name} {self.client.last_name} - {self.weight} kg at {self.recorded_at}"
 
@@ -111,7 +178,6 @@ class HeightRecord(models.Model):
     client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='height_records')
     height = models.FloatField()
     recorded_at = models.DateTimeField(auto_now_add=True)
-
     def __str__(self):
         return f"{self.client.name} {self.client.last_name} - {self.height} cm at {self.recorded_at}"
 
@@ -119,7 +185,6 @@ class WaistRecord(models.Model):
     client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='waist_records')
     waist = models.FloatField()
     recorded_at = models.DateTimeField(auto_now_add=True)
-
     def __str__(self):
         return f"{self.client.name} {self.client.last_name} - {self.waist} cm at {self.recorded_at}"
 
